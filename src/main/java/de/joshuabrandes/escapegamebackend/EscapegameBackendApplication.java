43bf64a9cbc26d4a45d82253f5506b9249f98e98
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2021-12-08 23:41:20 +0100

    initial commit

---
+++ src/main/java/de/joshuabrandes/escapegamebackend/EscapegameBackendApplication.java
@@ -0,0 +1,13 @@
+package de.joshuabrandes.escapegamebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapegameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscapegameBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/joshuabrandes/escapegamebackend/EscapegameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.joshuabrandes.escapegamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapegameBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
